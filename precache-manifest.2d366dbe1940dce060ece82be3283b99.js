c1051267687be1a1b773d078b1e61cfea53a538b
Ricardo Bastardo <rabastardo.11@est.ucab.edu.ve>
2020-02-02 23:19:23 -0400

    Updates

---
+++ precache-manifest.2d366dbe1940dce060ece82be3283b99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50710bebc8d74e3399397cac7d12b7a",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "78eed70681f1028573b4",
+    "url": "/robofriend/static/css/main.aabf1546.chunk.css"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriend/static/js/2.b0983e84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriend/static/js/2.b0983e84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "78eed70681f1028573b4",
+    "url": "/robofriend/static/js/main.4e219b30.chunk.js"
+  },
+  {
+    "revision": "73b0268b71309baed9b7",
+    "url": "/robofriend/static/js/runtime-main.2d85b6a6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
